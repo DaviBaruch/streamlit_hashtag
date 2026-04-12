f4e7752fa89a0691b6f24a88d236233964d1f761
Davi Baruch <davibaruchg@gmail.com>
2026-04-12 04:23:41 -0300

    Co-authored-by: Gabriel Pacheco <gabrielmepv@gmail.com> colocando gráfico e filtrando as ações da lista de ações

--- main.py
+++ main.py
@@ -2,24 +2,43 @@ import pandas as pd
 import streamlit as st      
 import yfinance as yf
 
+st.write("""
+# App Preço de Ações
+O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo dos anos
+""")
+
 def carregar_dados(empresa):
-    try:
-        dados_acao = yf.Ticker(empresa)
-        precos_acao = dados_acao.history(start='2016-01-01')
+    texto_tickers = " ".join(empresa)
+    dados_acao = yf.Tickers(texto_tickers)
+    precos_acao = dados_acao.history(start='2016-01-01')
+    # verifica se veio vazio
+    return precos_acao
 
-        # verifica se veio vazio
-        if precos_acao is None or precos_acao.empty:
-            st.error("Não foi possível carregar os dados da ação.")
-            return None
+# cria lista de carteiras
+acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "GGBR4", "BBDC4.SA", "ABEV3.SA", "BITCOIN-USD"]
 
-        precos_acao.reset_index(inplace=True)
-        return precos_acao
 
-    except Exception as e:
-        st.error(f"Erro ao buscar dados: {e}")
-        return None
+#variável para armazenar a ação selecionada
+dados = carregar_dados(acoes)
+# filtro correto
 
-dados = carregar_dados("ITUB4.SA")
 
-if dados is not None:
-    st.line_chart(dados.set_index("Date")["Close"])
+lista_acoes = st.multiselect("Selecione as ações para exibir o gráfico:", acoes)
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
+
+    dados = yf.download(lista_acoes, start='2016-01-01')
+
+    dados = dados["Close"]
+
+    # 🔥 GARANTE QUE SEMPRE SEJA DATAFRAME
+    if isinstance(dados, pd.Series):
+        dados = dados.to_frame()
+
+    dados.reset_index(inplace=True)
+
+st.line_chart(dados["Close"])
+
